ee9a17a1d241447556dad4e980b564f0e10f2688
Phong Nguyen <nguyendoanphong.it@gmail.com>
2017-10-02 21:02:37 +0700

    Configured a simple windows service using Topself

---
+++ src/Practical.Topself/Practical.Topself/DemoService.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical.Topself
+{
+    public class DemoService
+    {
+        private BackgroundWorker _worker;
+
+        public DemoService()
+        {
+            _worker = new BackgroundWorker()
+            {
+                WorkerSupportsCancellation = true
+            };
+            _worker.DoWork += worker_DoWork;
+            _worker.RunWorkerCompleted += worker_RunWorkerCompleted;
+        }
+
+        private void worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            Console.WriteLine(e.Cancelled);
+            Console.WriteLine(e.Error);
+            Console.WriteLine(e.Result);
+        }
+
+        private void worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            string input = "";
+            while (!_worker.CancellationPending)
+            {
+                input = Console.ReadLine();
+                Console.WriteLine(input);
+                Console.WriteLine(_worker.CancellationPending);
+
+                if (input == "cancel")
+                {
+                    _worker.CancelAsync();
+                }
+            }
+        }
+
+        public void Start()
+        {
+            Console.WriteLine("Service is starting.");
+            _worker.RunWorkerAsync();
+        }
+
+        public void Stop()
+        {
+            _worker.CancelAsync();
+            Console.WriteLine("Service was stopped.");
+        }
+    }
+}
---
+++ src/Practical.Topself/Practical.Topself/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace Practical.Topself
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HostFactory.Run(c =>
+            {
+                c.Service<DemoService>(service =>
+                {
+                    service.ConstructUsing(s => new DemoService());
+                    service.WhenStarted(s => s.Start());
+                    service.WhenStopped(s => s.Stop());
+                });
+
+                c.RunAsLocalSystem();
+                c.StartAutomatically();
+                c.SetDescription("My Simple Service");
+                c.SetDisplayName("My Simple Service");
+                c.SetServiceName("My Simple Service");
+                c.EnableServiceRecovery(x =>
+                {
+                    //First failure
+                    x.RestartService(1);
+                    //Second failure
+                    x.RestartService(1);
+                    //Subsequent failures
+                    x.RestartService(1);
+                });
+
+                //Service Dependencies
+                //https://topshelf.readthedocs.io/en/latest/configuration/config_api.html#service-dependencies
+            });
+        }
+    }
+}
